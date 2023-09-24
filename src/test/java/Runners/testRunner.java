98d7b38efe7853c92ba2dfe817f96eb75aaac8af
RustamRoz <rustamrozik626@gmail.com>
2023-09-24 11:37:15 +0300

    Features/Scenario

---
+++ src/test/java/Runners/testRunner.java
@@ -0,0 +1,17 @@
+package Runners;/*
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.testng.AbstractTestNGCucumberTests;
+
+@CucumberOptions
+        (
+                features = {"src/test/java/FeatureFiles"},
+                glue = {"StepDefinitions"},
+
+                plugin = {
+                        "html:target/cucumber-report",
+                        "json:target/cucumber.json",
+                }
+        )
+public class testRunner extends AbstractTestNGCucumberTests {
+}*/
